ad8a39508d6c9b4f5d54dc4eb1aa2a6915fc2e38
Paul Leveau <plawn.yay@juniorisep.com>
2020-04-06 20:30:25 +0200

    with new build

---
+++ frontend/build/precache-manifest.095224e6e9f978450fecdd6264fdac29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7dc8a2a83c311dc0afeaf73e55b1ae0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7fd9788ff0e2fc82049f",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "869329d53121da0950c6",
+    "url": "/static/js/2.7c24faa7.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/static/js/2.7c24faa7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fd9788ff0e2fc82049f",
+    "url": "/static/js/main.5f202c73.chunk.js"
+  },
+  {
+    "revision": "6060cd9ce4ad74fa104e",
+    "url": "/static/js/runtime-main.0f70afab.js"
+  }
+]);
