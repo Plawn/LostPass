c88a461d27cc3c2ce41f2e9716703ca2d5c6bee0
Paul Leveau <plawn.yay@juniorisep.com>
2020-04-06 21:41:29 +0200

    added shebang everywhere

--- install_backend.sh
+++ install_backend.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 cd backend
 pip3 install -r requirements.txt
--- start_standalone.sh
+++ start_standalone.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 echo "[WARN]: Using default config"
 echo "Serving on port 5000"
 echo "Requires redis to run on port 6379 on localhost"
